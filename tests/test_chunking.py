7328a1399d3f3f765450ebc330ce80897fd8a42c
Davidyz <hzjlyz@gmail.com>
2025-03-25 15:35:20 +0000

    test(cli): add tests for merging patterns and wildcard language

--- tests/test_chunking.py
+++ tests/test_chunking.py
@@ -102,6 +102,48 @@ def bar():
     os.remove(test_file)
 
 
+def test_treesitter_chunker_filter_merging():
+    chunker = TreeSitterChunker(
+        Config(chunk_size=30, chunk_filters={"python": [".*foo.*", ".*bar.*"]})
+    )
+
+    test_content = r"""
+def foo():
+    return "foo"
+
+def bar():
+    return "bar"
+    """
+
+    with tempfile.NamedTemporaryFile(mode="w", delete=False, suffix=".py") as tmp_file:
+        tmp_file.write(test_content)
+        test_file = tmp_file.name
+
+    chunks = list(chunker.chunk(test_file))
+    assert chunks == []
+    os.remove(test_file)
+
+
+def test_treesitter_chunker_filter_wildcard():
+    chunker = TreeSitterChunker(Config(chunk_size=30, chunk_filters={"*": [".*foo.*"]}))
+
+    test_content = r"""
+def foo():
+    return "foo"
+
+def bar():
+    return "bar"
+    """
+
+    with tempfile.NamedTemporaryFile(mode="w", delete=False, suffix=".py") as tmp_file:
+        tmp_file.write(test_content)
+        test_file = tmp_file.name
+
+    chunks = list(chunker.chunk(test_file))
+    assert chunks == ['def bar():\n    return "bar"']
+    os.remove(test_file)
+
+
 def test_treesitter_chunker_lua():
     chunker = TreeSitterChunker(Config(chunk_size=30))
     test_content = r"""
