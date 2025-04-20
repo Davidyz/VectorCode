d1651887fb0d6d0e0a1d48db5a3dce986d24a934
Zhe Yu <zcabzyu@ucl.ac.uk>
2025-04-20 17:29:12 +0800

    dynamically dispatch reranker class

--- src/vectorcode/subcommands/query/reranker/__init__.py
+++ src/vectorcode/subcommands/query/reranker/__init__.py
@@ -1,4 +1,5 @@
 import logging
+import sys
 
 from vectorcode.cli_utils import Config
 
@@ -12,18 +13,21 @@ logger = logging.getLogger(name=__name__)
 
 
 def get_reranker(configs: Config) -> RerankerBase:
-    if configs.reranker == "NaiveReranker" or not configs.reranker:
-        return NaiveReranker(configs)
-    elif configs.reranker == "CrossEncoderReranker":
-        return CrossEncoderReranker(configs)
-    else:
-        logger.warning(
-            f"""
-"reranker" option should be set to one of the following: {list(i for i in __all__ if i != "RerankerBase")}.
-To choose a custom reranker model, you can set the "model_name_or_path" key in the "reranker_params" option to the name/path of the model.
-The old configuration syntax will be deprecated in v0.6.0
+    if configs.reranker and hasattr(sys.modules[__name__], configs.reranker):
+        # dynamic dispatch
+        return getattr(sys.modules[__name__], configs.reranker)(configs)
+
+    # TODO: replace the following with an Exception before the release of 0.6.0.
+    logger.warning(
+        f""""reranker" option should be set to one of the following: {list(i for i in __all__ if i != "RerankerBase")}.
+To choose a CrossEncoderReranker model, you can set the "model_name_or_path" key in the "reranker_params" option to the name/path of the model.
+To use NaiveReranker, set the "reranker" option to "NaiveReranker".
+The old configuration syntax will be DEPRECATED in v0.6.0
                 """
-        )
+    )
+    if not configs.reranker:
+        return NaiveReranker(configs)
+    else:
         configs.reranker_params.update({"model_name_or_path": configs.reranker})
         configs.reranker = "CrossEncoderReranker"
         return CrossEncoderReranker(
