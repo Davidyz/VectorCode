af841972aa4acc91b1bdb1ac78f8610df86b63eb
Davidyz <hzjlyz@gmail.com>
2025-04-30 09:59:54 +0800

    add default options for extension, as well as slash command.

--- lua/codecompanion/_extensions/vectorcode/init.lua
+++ lua/codecompanion/_extensions/vectorcode/init.lua
@@ -3,6 +3,7 @@
 ---@class VectorCode.CodeCompanion.ExtensionOpts
 ---@field add_tools boolean
 ---@field tool_opts VectorCode.CodeCompanion.ToolOpts
+---@field add_slash_command boolean
 
 local vc_config = require("vectorcode.config")
 
@@ -10,14 +11,22 @@ local vc_config = require("vectorcode.config")
 local M = {
   ---@param opts VectorCode.CodeCompanion.ExtensionOpts
   setup = vc_config.check_cli_wrap(function(opts)
+    opts = vim.tbl_deep_extend(
+      "force",
+      { add_tools = true, add_slash_command = false },
+      opts or {}
+    )
     local cc_config = require("codecompanion.config").config
+    local cc_integration = require("vectorcode.integrations").codecompanion.chat
     if opts.add_tools then
       cc_config.strategies.chat.tools["vectorcode"] = {
         description = "Run VectorCode to retrieve the project context.",
-        callback = require("vectorcode.integrations").codecompanion.chat.make_tool(
-          opts.tool_opts
-        ),
+        callback = cc_integration.make_tool(opts.tool_opts),
       }
+    end
+    if opts.add_slash_command then
+      cc_config.strategies.chat.slash_commands["vectorcode"] =
+        cc_integration.make_slash_command()
     end
   end),
 }
