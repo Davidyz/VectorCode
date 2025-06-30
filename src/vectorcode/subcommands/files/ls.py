8d775f432fbd105123e6281f8d86992776e2c227
Zhe Yu <zcabzyu@ucl.ac.uk>
2025-06-30 10:44:18 +0800

    feat(cli): add `files` subcommand for file manipulation

---
+++ src/vectorcode/subcommands/files/ls.py
@@ -0,0 +1,30 @@
+import json
+import logging
+
+from chromadb.api.types import IncludeEnum
+
+from vectorcode.cli_utils import Config
+from vectorcode.common import ClientManager, get_collection
+
+logger = logging.getLogger(name=__name__)
+
+
+async def ls(configs: Config) -> int:
+    async with ClientManager().get_client(configs=configs) as client:
+        try:
+            collection = await get_collection(client, configs, False)
+        except ValueError:
+            logger.error(f"There's no existing collection at {configs.project_root}.")
+            return 1
+        meta = (await collection.get(include=[IncludeEnum.metadatas]))["metadatas"]
+        if meta is None:
+            logger.warning("Failed to fetch metadatas from the database.")
+            return 0
+        paths: list[str] = list(set(str(m.get("path")) for m in meta))
+        paths.sort()
+        if configs.pipe:
+            print(json.dumps(list(paths)))
+        else:
+            for p in paths:
+                print(p)
+    return 0
