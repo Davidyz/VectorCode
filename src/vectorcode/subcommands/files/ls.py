70103077c7acbca127a8c15fa398d97c219edb0b
Zhe Yu <zcabzyu@ucl.ac.uk>
2025-07-01 12:22:48 +0800

    refactor(cli): refactor `files ls` to reuse file listing logic

--- src/vectorcode/subcommands/files/ls.py
+++ src/vectorcode/subcommands/files/ls.py
@@ -1,12 +1,23 @@
 import json
 import logging
 
+from chromadb.api.models.AsyncCollection import AsyncCollection
 from chromadb.api.types import IncludeEnum
 
 from vectorcode.cli_utils import Config
 from vectorcode.common import ClientManager, get_collection
 
 logger = logging.getLogger(name=__name__)
+
+
+async def list_files(collection: AsyncCollection) -> list[str]:
+    meta = (await collection.get(include=[IncludeEnum.metadatas])).get("metadatas")
+    if meta is None:
+        logger.warning("Failed to fetch metadatas from the database.")
+        return []
+    paths: list[str] = list(set(str(m.get("path")) for m in meta))
+    paths.sort()
+    return paths
 
 
 async def ls(configs: Config) -> int:
@@ -16,12 +27,7 @@ async def ls(configs: Config) -> int:
         except ValueError:
             logger.error(f"There's no existing collection at {configs.project_root}.")
             return 1
-        meta = (await collection.get(include=[IncludeEnum.metadatas])).get("metadatas")
-        if meta is None:
-            logger.warning("Failed to fetch metadatas from the database.")
-            return 0
-        paths: list[str] = list(set(str(m.get("path")) for m in meta))
-        paths.sort()
+        paths = await list_files(collection)
         if configs.pipe:
             print(json.dumps(list(paths)))
         else:
