0361052f607eed20313297729eebbb6b8d8be7ef
Zhe Yu <zcabzyu@ucl.ac.uk>
2025-08-12 11:40:45 +0800

    feat(nvim): use pre_hook to prepare the embeddings.

--- lua/vectorcode/integrations/codecompanion/prompts/nvim.lua
+++ lua/vectorcode/integrations/codecompanion/prompts/nvim.lua
@@ -1,5 +1,26 @@
 ---@module "codecompanion"
 
+local config = require("vectorcode.config")
+local prepare = function(rtp)
+  vim.notify(
+    "Vectorising neovim runtime files...",
+    vim.log.levels.INFO,
+    config.notify_opts
+  )
+  require("vectorcode.integrations.codecompanion.prompts").vectorise_files(
+    vim.fs.joinpath(rtp, "**/*.lua"),
+    rtp,
+    function(result, _, _, _)
+      if result ~= nil and not vim.tbl_isempty(result) then
+        vim.schedule_wrap(vim.notify)(
+          string.format("Added %d files to the database!", result.add or 0),
+          vim.log.levels.INFO,
+          config.notify_opts
+        )
+      end
+    end
+  )
+end
 return require("vectorcode.config").check_cli_wrap(function()
   local constants = require("codecompanion.config").config.constants
   local rtp = vim.fs.normalize(vim.env.VIMRUNTIME)
@@ -17,12 +38,17 @@ return require("vectorcode.config").check_cli_wrap(function()
       vim.log.levels.ERROR
     )
   end
+
   return {
     name = "Neovim Assistant",
     prompts = {
       strategy = "chat",
       description = "Use VectorCode to index and query from neovim documentation and lua runtime.",
-
+      opts = {
+        pre_hook = function()
+          prepare(rtp)
+        end,
+      },
       prompts = {
         {
           role = constants.SYSTEM_ROLE,
@@ -30,10 +56,11 @@ return require("vectorcode.config").check_cli_wrap(function()
             [[You are an neovim expert.
 You will be given tools to index and query from the neovim runtime library.
 This will include lua APIs for the neovim runtime and documentation for the neovim build that the user is running.
-Use the tools to explain the user's questions related to neovim.
 The runtime files are stored in `%s`.
 You can ONLY use the vectorcode tools to interact with these files or directory.
 DO NOT attempt to read from or write into this directory.
+When the user asked a question that is not part of a previous query tool call, make a new query using new keywords that are directly relevant to the new question.
+If the tool returns an error that says the collection doesn't exist, it's because the files are still being indexed, and you should ask the user to wait for it to finish.
 ]],
             rtp
           ),
@@ -42,13 +69,15 @@ DO NOT attempt to read from or write into this directory.
           role = constants.USER_ROLE,
           content = string.format(
             [[
-You are given the @{vectorcode_vectorise} and @{vectorcode_query} tools.
-Vectorrise all lua files and `*.txt` files under this directory using the `vectorcode_vectorise` tool. Use wildcards to match all lua and `txt` files. Use absolute paths when supplying paths to the vectorcode_vectorise tool;
+You are given the @{vectorcode_query} tool.
 Use `%s` as the value of the `project_root` argument;
-When you're done, I'll be asking you questions related to these documents.
+I'll be asking you questions related to these documents.
 Use the vectorcode_query tool to query from the project root and answer my question.
+
+Here's my question:
+
+- 
 ]],
-            rtp,
             rtp
           ),
         },
