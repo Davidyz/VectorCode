129ebd46495b56234c5247165577a574f0b342ce
Zhe Yu <zcabzyu@ucl.ac.uk>
2025-09-07 20:30:15 +0800

    docs about database connectors.

--- src/vectorcode/database/base.py
+++ src/vectorcode/database/base.py
@@ -19,6 +19,28 @@ from vectorcode.subcommands.query.types import QueryResult
 logger = logging.getLogger(name=__name__)
 
 
+"""
+For developers:
+
+To implement a custom database connector, you should inherit the following 
+`DatabaseConnectorBase` class and implement ALL abstract methods. 
+
+You should also try to wrap the exceptions with the ones in 
+`src/vectorcode/database/errors.py` where appropriate, because this helps the 
+CLI/LSP/MCP interfaces to handle some common edge cases. To do this, you should do the following in a try-except block:
+```python
+from vectorcode.database.errors import CollectionNotFoundError
+
+try:
+    some_action_here()
+except SomeCustomException as e:
+    raise CollectionNotFoundError("The collection was not found.") from e
+```
+This will preserve the correct call stack in the error message and makes debugging 
+easier.
+"""
+
+
 class DatabaseConnectorBase(ABC):  # pragma: nocover
     @classmethod
     def create(cls, configs: Config):
